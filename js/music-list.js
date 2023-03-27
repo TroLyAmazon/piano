880352b1b99f61f986f9bd1a297cf1ad32623a4a
Trợ Lý Amazon <82072897+TroLyAmazon@users.noreply.github.com>
2023-03-27 18:02:24 +0700

    Update music-list.js

--- js/music-list.js
+++ js/music-list.js
@@ -43,13 +43,13 @@ let allMusic = [
   {
     name: "Kill Bill ",
     artist: "SZA",
-    img: "Logo",
+    img: "KillBill",
     src: "KillBill"
   },
   {
     name: "Flower Day 꽃날",
     artist: "Seo Woong Seok",
-    img: "Logo",
+    img: "FlowerDayHQ",
     src: "FlowerDayHQ"
   } 
   // like this paste it and remember to give comma after ending of this bracket }
